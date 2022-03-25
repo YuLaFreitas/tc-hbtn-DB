5cd6577a476f43ae0ca5d21a4bcffc8f4619837d
YuLaFreitas <iruytj@gmail.com>
2022-03-25 16:36:56 -0300

    Arquivo chema gerado

---
+++ 0x02/java_jdbc_manipulation/sql_schema_sqlite_database_marco_2022.sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE cliente (id integer primary key autoincrement,cpf text,rg text,nome text,idade integer);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
