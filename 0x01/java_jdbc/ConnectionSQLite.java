784e62409b5fb6e83f973a550dfc41ceb6b1e702
YuLaFreitas <iruytj@gmail.com>
2022-03-25 11:10:42 -0300

    Criado o primero banco

---
+++ 0x01/java_jdbc/ConnectionSQLite.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionSQLite {
+    public static void main(String[] args){
+        initConnection();
+    }
+
+    public static void initConnection(){
+        Connection cnn = null;
+
+        try {
+            cnn = DriverManager.getConnection("jdbc:sqlite:sqlite_database_2022.db");
+            System.out.println(cnn);
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if(cnn != null) {
+                    cnn.close();
+                }
+                else {
+                    System.out.println("Probelam com a conexão!");
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
